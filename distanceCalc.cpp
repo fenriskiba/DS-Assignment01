75d4ad322f278e9de6e377acc5a3c5726b4ddc46
Grant Carroll <gcarroll2603@gmail.com>
2013-09-09 17:47:52 -0400

    Added basic prompt for commands

--- distanceCalc.cpp
+++ distanceCalc.cpp
@@ -6,8 +6,35 @@
 
 using namespace std;
 
+void displayPrompt();
+
 int main()
 {
+	bool running = true;
+	
+	while(running)
+	{
+		displayPrompt();
+	}
+
 	return 0;
+}
+
+void displayPrompt()
+{
+	//Display basic Prompt
+	cout << "*************************************" << endl
+		<< "Implementation Options:" << endl
+		<< "A.Use an array-based list implementation" << endl
+		<< "B.Use a linked-list implementation" << endl << endl
+		<< "Operation Options:" << endl
+		<< "1. Insert a record" << endl
+		<< "2. Search for a record by name" << endl
+		<< "3. Search for a record by coordinate" << endl
+		<< "4. Delete a record by name" << endl
+		<< "5. Delete a record by coordinate" << endl
+		<< "6. Print records within a given distance of a specified location" << endl
+		<< "7. Print all records" << endl
+		<< "8. Exit" << endl;	
 }
 
